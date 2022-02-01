e2762dff5604520c6c179e5dbcf3cac69b4656a8
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-02-01 18:45:29 +0530

    use getStaticProps to for GET

---
+++ components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'next/link';
+import { MDBBtn } from "mdb-react-ui-kit";
+
+function Navbar() {
+    return (
+        <nav className='navbar container'>
+            <Link className="navbar-brand" href="/">Superhero Identity</Link>
+            <Link className="navbar-brand" href="/new" passHref><MDBBtn>New Identity</MDBBtn></Link>
+        </nav>
+    );
+}
+
+export default Navbar;
