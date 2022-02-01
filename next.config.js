9ff27acb6e970407aaf4311d55e6327723d2a523
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-02-01 13:27:04 +0530

    create api for get and post

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    MONGO_URL: "mongodb://localhost:27017/nextjs/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+    MONGO_URL: "mongodb://localhost:27017/nextjs?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
   }
 }
