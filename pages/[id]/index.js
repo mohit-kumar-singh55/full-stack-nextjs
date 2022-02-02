9f7b27cb1484460259f269c79383895f3b01e61f
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-02-02 17:29:24 +0530

    DELETE

--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBBtn } from 'mdb-react-ui-kit';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const axios = require('axios').default;
 
 export default function EachHero({ heros }) {
+    const router = useRouter();
+    const heroId = router.query.id;
+
+    const deleteHero = async () => {
+        try {
+            const deleteHero = await axios(`http://localhost:3000/api/hero/${heroId}`, {
+                method: "DELETE"
+            })
+            router.push("/");
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <div className="container" >
             <h1 className='display-2'>Identiy of hero</h1>
@@ -15,7 +30,7 @@ export default function EachHero({ heros }) {
                         <MDBCardText>
                             {heros.realName}
                         </MDBCardText>
-                        <MDBBtn className='btn btn-danger'>Delete hero</MDBBtn>
+                        <MDBBtn className='btn btn-danger' onClick={deleteHero}>Delete hero</MDBBtn>
                     </MDBCardBody>
                 </MDBCard>
             </div>
